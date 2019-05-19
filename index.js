a22cba981cabe7878ea90596d763b44dbbbe9be2
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-19 20:16:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( resp ) {
+      return resp.json()
+    } )
+    .then(jsonData => {
+      document.body.innerHTML = jsonData[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
+
